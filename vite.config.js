3363f9459da36980df0e80a933027c48674118ec
s.akai <s.akai@sint.co.jp>
2025-12-04 14:27:24 +0900

    開発環境のbaseパスを修正（空白画面の原因を解消）

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/English-Grammar-Mastery-App/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
